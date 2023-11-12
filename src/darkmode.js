64031dab2b948db136120d5b4b330c1a426bafae
Gileadejls <gileadelima16@gmail.com>
2023-11-11 22:33:09 -0300

    adicionando botão de voltar ao topo

--- src/darkmode.js
+++ src/darkmode.js
@@ -17,3 +17,20 @@ moon.addEventListener("click", ()=>{
     body.classList.add("dark")
     categ.src = "../images/icons/categoria(1).png"
 })
+
+const backtop = document.querySelector("#backtotop")
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > 100){
+        console.log("aaaaaaa")
+        backtop.classList.remove('invisible')
+        backtop.classList.remove('bottom-14')
+        backtop.classList.add('bottom-20')
+        backtop.classList.add('visible')
+    }else{
+        backtop.classList.remove('visible')
+        backtop.classList.add('invisible')
+        backtop.classList.remove('bottom-20')
+        backtop.classList.add('bottom-14')
+    }
+})
