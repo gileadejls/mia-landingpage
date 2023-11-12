ddf0ec5b6bb23f3bd0139f0ac7c2e34f5a5b3945
Gileadejls <gileadelima16@gmail.com>
2023-11-11 21:32:33 -0300

    resolvendo problema do espaço no icone de tema

--- src/darkmode.js
+++ src/darkmode.js
@@ -4,12 +4,16 @@ const body = document.querySelector('html')
 //icons
 const sun = document.querySelector('#sun')
 const moon = document.querySelector('#moon')
+const categ = document.querySelector('#category')
+
 
 
 sun.addEventListener('click', ()=>{
     body.classList.remove("dark")
+    categ.src = "../images/icons/categoria.png"
 })
 
 moon.addEventListener("click", ()=>{
     body.classList.add("dark")
+    categ.src = "../images/icons/categoria(1).png"
 })
